a27064a5605abd703389e5c91aed39f289019b9d
Max Ognenoi <maksymohnenyi>
2024-03-25 23:07:08 -0400

    update5

--- src/components/NavNotice.jsx
+++ src/components/NavNotice.jsx
@@ -1,7 +1,28 @@
 import React from "react";
 
 function NavNotice() {
-  return <div></div>;
+  return (
+    <li className="nav-item dropdown">
+      <a className="nav-link nav-icon" href="#" data-bs-toggle="dropdown">
+        <i className="bi bi-bell"></i>
+        <span className="badge bg-primary badge-number">4</span>
+      </a>
+
+      <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow notifications">
+        <li className="dropdown-header">
+          You have 4 new notifications
+          <a href="#">
+            <span className="badge rounded-pill bg-primary p-2 ms-2">
+              View all
+            </span>
+          </a>
+        </li>
+        <li>
+          <hr className="dropdown-divider" />
+        </li>
+      </ul>
+    </li>
+  );
 }
 
 export default NavNotice;
