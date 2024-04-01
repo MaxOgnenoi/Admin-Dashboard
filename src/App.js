189a4cc6ae93c9b33c6f7d63b3b8ec16eca0f77c
Max Ognenoi <maksymohnenyi>
2024-03-31 22:21:44 -0400

    update29

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "./App.css";
 import Header from "./components/Header";
+import SideBar from "./components/SideBar";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <SideBar />
+    </>
+  );
 }
 
 export default App;
