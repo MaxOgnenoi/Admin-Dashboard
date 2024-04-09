c0bbc0046074a540ed53d3f0dd5fb1af384e6333
Max Ognenoi <maksymohnenyi>
2024-04-08 23:54:16 -0400

    update42

--- src/components/PageTitle.jsx
+++ src/components/PageTitle.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
-import './pageTitle.css'
+import React from "react";
+import "./pageTitle.css";
 
-function PageTitle() {
+function PageTitle({page}) {
   return (
-    <div>
-      
+    <div className="pagetitle">
+      <h1>{page}</h1>
+      <nav>
+        <ol className="breadcrumb">
+          <li className="breadcrumb-item">
+            <a href="/">
+              <i className="bi bi-house-door"></i>
+            </a>
+          </li>
+          <li className="breadcrumb-item active">{page}</li>
+        </ol>
+      </nav>
     </div>
-  )
+  );
 }
 
-export default PageTitle
+export default PageTitle;
