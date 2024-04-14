7f947d38bf91cff28313a549db6bb48905844b93
Max Ognenoi <maksymohnenyi>
2024-04-13 23:11:34 -0400

    update58

--- src/components/ReportCharts.jsx
+++ src/components/ReportCharts.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Chart from "react-apexcharts";
 
 function ReportCharts() {
+  const [data, setData] = useState({
+    series: [
+      {
+        name: "Sales",
+        data: [31, 40, 28, 51, 42, 82, 56],
+      },
+      {
+        name: "Revenue",
+        data: [11, 32, 45, 32, 34, 52, 41],
+      },
+      {
+        name: "Customers",
+        data: [15, 11, 32, 18, 42, 9, 24],
+      },
+    ],
+  });
   return <div></div>;
 }
 
