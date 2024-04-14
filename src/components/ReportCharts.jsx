cc584efac7e28b0f107178be8a0e45d61513bdb7
Max Ognenoi <maksymohnenyi>
2024-04-13 23:15:48 -0400

    update59

--- src/components/ReportCharts.jsx
+++ src/components/ReportCharts.jsx
@@ -17,6 +17,28 @@ function ReportCharts() {
         data: [15, 11, 32, 18, 42, 9, 24],
       },
     ],
+    options: {
+      chart: {
+        height: 350,
+        type: "area",
+        toolbar: {
+          show: false,
+        },
+      },
+      markers: {
+        size: 4,
+      },
+      colors: ["#4154f1", "#2eca6a", "#ff771d"],
+      fill: {
+        type: "gradient",
+        gradient: {
+          shadeIntensity: 1,
+          opacityFrom: 0.3,
+          opacityTo: 0.4,
+          stops: [0, 90, 100],
+        },
+      },
+    },
   });
   return <div></div>;
 }
